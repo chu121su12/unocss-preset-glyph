39d167c4e41df546ece986e8f06c3aa9427c21b9
Saya <chu121su12@gmail.com>
2021-12-29 15:40:45 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import type { CSSValues, Preset } from '@unocss/core'
 import { warnOnce } from '@unocss/core'
-import { subsetFont } from './utils'
 
 /**
  * @public
@@ -103,4 +102,34 @@ export const presetGlyph = (options: GlyphOptions = {}): Preset => {
       { layer },
     ]],
   }
+}
+
+const isNode = typeof process < 'u' && typeof process.stdout < 'u'
+
+let Fontmin
+if (isNode) {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  Fontmin = require('fontmin')
+}
+
+function subsetFont(path: string, glyphs: string) {
+  return new Promise<string | undefined>((resolve) => {
+    if (!isNode)
+      return resolve(undefined)
+
+    const f = new Fontmin().src(path)
+
+    if (path.match(/\.otf$/))
+      f.use(Fontmin.otf2ttf())
+
+    f.use(Fontmin.glyph({ text: glyphs }))
+      .use(Fontmin.css({ base64: true }))
+
+    f.run((err: any, files: any[]) => {
+      if (err)
+        return resolve(undefined)
+      const fontData = files[1].contents.toString().replace(/^[\s\S]*?;base64,([^)]+)\)[\s\S]*$/, '$1')
+      resolve(fontData)
+    })
+  })
 }
